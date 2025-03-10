a2630e27b4cde2e6e44cc96c78685e8a98e1bee2
Valentyn Lysenko <valentynlysenco@gmail.com>
2025-03-10 20:30:43 +0200

    feat: solve a fromJSON should return the object of specified type from JSON representation

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -220,8 +220,11 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const res = Object.create(proto);
+  const parsed = JSON.parse(json);
+  Object.assign(res, parsed);
+  return res;
 }
 
 /**
